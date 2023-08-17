648cf2d5ca55c8316ba5d355ddfc42bc54b07709
Bebo7897 <81861464+Bebo7897@users.noreply.github.com>
2023-08-17 09:56:21 -0400

    added video, some css, and js animation

--- BasicHTMLvs/wwwroot/js/site.js
+++ BasicHTMLvs/wwwroot/js/site.js
@@ -2,3 +2,24 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+var i = 0;
+function move() {
+    if (i == 0) {
+        i = 1;
+        var elem = document.getElementById("myBar");
+        var width = 1;
+        var id = setInterval(frame, 500);
+        function frame() {
+            if (width >= 100) {
+                clearInterval(id);
+                i = 0;
+            } else {
+                width++;
+                elem.style.width = width + "%";
+                var double = width * 2;
+                elem.innerHTML = double + "&deg;F";
+            }
+        }
+    }
+}
